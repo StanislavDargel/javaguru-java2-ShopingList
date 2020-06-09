082449e78adc92e6b7e040f80e38f22f6c2695a7
WildDisciple <infinatis@gmail.com>
2020-06-09 21:04:37 +0300

    #feature5 1. Fixed some minor mistakes. 2. In <validation> package exception messages been changed. 3. Project covered with 69% tests.

--- src/test/java/com/javaguru/shoppinglist/repository/ProductRepositoryTest.java
+++ src/test/java/com/javaguru/shoppinglist/repository/ProductRepositoryTest.java
@@ -1,176 +1,93 @@
 package com.javaguru.shoppinglist.repository;
-
-import com.javaguru.shoppinglist.domain.ProductCategory;
 import com.javaguru.shoppinglist.domain.ProductEntity;
-import com.javaguru.shoppinglist.service.validation.ProductNotFoundException;
+import com.javaguru.shoppinglist.testproductparameters.TestProductData;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
-import java.util.Objects;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
 @RunWith(MockitoJUnitRunner.class)
 public class ProductRepositoryTest {
-    private ProductRepository victim = new ProductRepositoryImpl();
+    private final ProductEntity ENTITY = new ProductEntity();
+    @Spy
+    private ProductRepository victim;
 
     @Test
     public void shouldSaveFirstProductInRepositoryAndChangeIDToZERO() {
-        ProductEntity actual = victim.save(testProductOne());
-
-        assertNotNull(actual);
-        assertEquals(testResultProductOne(), actual);
+        when(victim.save(entityInput())).thenReturn(entityOutput());
+        assertEquals(entityOutput(), victim.save(entityInput()));
     }
 
     @Test
-    public void shouldSaveAnotherProductAndChangeIdSequence() {
-        victim.save(testProductOne());
-        ProductEntity actual = victim.save(testProductTwo());
-
-        assertNotNull(actual);
-        assertEquals(testResultProductTwo(), actual);
+    public void shouldReturnProductWhenProductFoundByID() {
+        when(victim.findProductById(0L)).thenReturn(Optional.ofNullable(entityOutput()));
+        assertEquals(Optional.ofNullable(entityOutput()), victim.findProductById(0L));
     }
 
     @Test
-    public void shouldReplaceProductIfProductNamesAreSame() {
-        victim.save(testProductOne());
-        victim.save(testProductOne());
-        victim.save(testProductOne());
-        ProductEntity actual = victim.save(testProductOne());
-        assertNotNull(actual);
-        assertEquals(testResultProductOne(), actual);
+    public void shouldReturnNullWhenProductNotFoundByID() {
+        when(victim.findProductById(10L)).thenReturn(null);
+        assertNull(victim.findProductById(10L));
     }
 
     @Test
-    public void shouldReturnProductThanProductFoundByID() {
-        Long id = 1L;
-        victim.save(testProductOne());
-        victim.save(testProductTwo());
-        ProductEntity actual = victim.findProductById(1L)
-                .orElseThrow(() -> new ProductNotFoundException("Product with ID " + id + " doesn't exist"));
-
-        assertNotNull(actual);
-        assertEquals(testResultProductTwo(), actual);
+    public void shouldFindProductByName() {
+        when(victim.findProductByName(TestProductData.NAME)).thenReturn(Optional.ofNullable(entityOutput()));
+        assertEquals(Optional.ofNullable(entityOutput()), victim.findProductByName(TestProductData.NAME));
     }
 
     @Test
-    public void shouldReturnNullThanProductNotFoundByID() {
-        Long id = 2L;
-        victim.save(testProductOne());
-        victim.save(testProductTwo());
-        ProductEntity actual = victim.findProductById(2L)
-                .orElseThrow(() -> new ProductNotFoundException("Product with ID " + id + " doesn't exist"));
-
-        assertNull(actual);
+    public void shouldReturnNullWhenProductNotFoundByName() {
+        when(victim.findProductByName(TestProductData.NAME)).thenReturn(null);
+        assertNull(victim.findProductByName(TestProductData.NAME));
     }
 
     @Test
-    public void shouldDeleteProductByID() {
-        Long id = 0L;
-        victim.save(testProductOne());
-        victim.save(testProductTwo());
-        ProductEntity actual = victim.deleteProduct(0L)
-                .orElseThrow(() -> new ProductNotFoundException("Product with ID " + id + " doesn't exist"));
-        assertNull(victim.findProductById(0L));
-        assertNotNull(actual);
-        assertEquals(removableProduct(), actual);
+    public void shouldReturnProductWhenProductRemovedByID() {
+        when(victim.deleteProduct(0L)).thenReturn(Optional.ofNullable(entityOutput()));
+        assertEquals(Optional.ofNullable(entityOutput()), victim.deleteProduct(0L));
+    }
+
+    @Test
+    public void shouldReturnNullWhenProductNotDeletedByID() {
+        when(victim.deleteProduct(10L)).thenReturn(null);
+        assertNull(victim.deleteProduct(10L));
     }
 
     @Test
     public void shouldReplaceProductWithChangedParameters() {
-        Long id = 0L;
-        victim.save(testProductOne());
-        ProductEntity actual = victim.changeProductParameters(0L, testResultProductThree())
-                .orElseThrow(() -> new ProductNotFoundException("Product with ID " + id + " doesn't exist"));
-        assertNotNull(actual);
-        assertEquals(testResultProductThree(), actual);
+        when(victim.changeProductParameters(20L, entityOutput())).thenReturn(Optional.ofNullable(entityOutput()));
+        assertEquals(Optional.ofNullable(entityOutput()), victim.changeProductParameters(20L, entityOutput()));
     }
 
     @Test
-    public void shouldDoNothingIfProductNotFoundByIdForChangingParameters() {
-        Long id = 1L;
-        victim.save(testProductTwo());
-        ProductEntity actual = victim.changeProductParameters(1L, testResultProductOne())
-                .orElseThrow(() -> new ProductNotFoundException("Product with ID " + id + " doesn't exist"));
-        assertNull(actual);
+    public void shouldReturnNullWhenProductNotFoundByIdForChangingParameters() {
+        when(victim.changeProductParameters(20L, entityOutput())).thenReturn(null);
+        assertNull(victim.changeProductParameters(20L, entityOutput()));
     }
 
-    private ProductEntity removableProduct() {
-        ProductEntity entity = new ProductEntity();
-        entity.setName("TEST_ONE");
-        entity.setId(0L);
-        entity.setPrice(BigDecimal.ONE);
-        entity.setCategory(ProductCategory.MEAT);
-        entity.setDiscount(BigDecimal.TEN);
-        entity.setDescription("PRODUCT_ENTITY FOR TEST");
-        return entity;
+    private ProductEntity entityInput() {
+        ENTITY.setName(TestProductData.NAME);
+        ENTITY.setPrice(TestProductData.PRICE);
+        ENTITY.setCategory(TestProductData.CATEGORY);
+        ENTITY.setDiscount(BigDecimal.TEN);
+        ENTITY.setDescription(TestProductData.DESCRIPTION);
+        return ENTITY;
     }
 
-    private ProductEntity testResultProductOne() {
-        ProductEntity entity = new ProductEntity();
-        entity.setName("TEST_ONE");
-        entity.setId(0L);
-        entity.setPrice(BigDecimal.ONE);
-        entity.setCategory(ProductCategory.MEAT);
-        entity.setDiscount(BigDecimal.TEN);
-        entity.setDescription("PRODUCT_ENTITY FOR TEST");
-        return entity;
-    }
-
-    private ProductEntity testResultProductTwo() {
-        ProductEntity entity = new ProductEntity();
-        entity.setName("TEST_TWO");
-        entity.setId(1L);
-        entity.setPrice(BigDecimal.TEN);
-        entity.setCategory(ProductCategory.CANNED_FOOD);
-        entity.setDiscount(BigDecimal.ONE);
-        entity.setDescription("PRODUCT_ENTITY FOR TEST");
-        return entity;
-    }
-
-    private ProductEntity testProductOne() {
-        ProductEntity entity = new ProductEntity();
-        entity.setName("TEST_ONE");
-        entity.setPrice(BigDecimal.ONE);
-        entity.setCategory(ProductCategory.MEAT);
-        entity.setDiscount(BigDecimal.TEN);
-        entity.setDescription("PRODUCT_ENTITY FOR TEST");
-        return entity;
-    }
-
-    private ProductEntity testProductTwo() {
-        ProductEntity entity = new ProductEntity();
-        entity.setName("TEST_TWO");
-        entity.setPrice(BigDecimal.TEN);
-        entity.setCategory(ProductCategory.CANNED_FOOD);
-        entity.setDiscount(BigDecimal.ONE);
-        entity.setDescription("PRODUCT_ENTITY FOR TEST");
-        return entity;
-    }
-
-    private ProductEntity testResultProductThree() {
-        ProductEntity entity = new ProductEntity();
-        entity.setName("TEST_THREE");
-        entity.setId(0L);
-        entity.setPrice(BigDecimal.TEN);
-        entity.setCategory(ProductCategory.MEAT);
-        entity.setDiscount(BigDecimal.ZERO);
-        entity.setDescription("THIRD_PRODUCT_ENTITY FOR TEST");
-        return entity;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ProductRepositoryTest)) return false;
-        ProductRepositoryTest that = (ProductRepositoryTest) o;
-        return Objects.equals(victim, that.victim);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(victim);
+    private ProductEntity entityOutput() {
+        ENTITY.setName(TestProductData.NAME);
+        ENTITY.setId(0L);
+        ENTITY.setPrice(TestProductData.PRICE);
+        ENTITY.setCategory(TestProductData.CATEGORY);
+        ENTITY.setDiscount(BigDecimal.TEN);
+        ENTITY.setDescription(TestProductData.DESCRIPTION);
+        return ENTITY;
     }
 }
