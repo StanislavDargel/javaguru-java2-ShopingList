6c780008f0b4d788936d1a7616c4d7e543e56ea8
WildDisciple <infinatis@gmail.com>
2020-08-17 15:14:56 +0300

    Minor Fixes Into ProductDescriptionInput.java

--- src/main/java/com/javaguru/shoppinglist/console/productparameters/ProductDescriptionInput.java
+++ src/main/java/com/javaguru/shoppinglist/console/productparameters/ProductDescriptionInput.java
@@ -12,28 +12,24 @@ public class ProductDescriptionInput implements ProductParameters {
     @Override
     public void inputParameter(ProductDTO productDTO) {
         Scanner scanner = new Scanner(System.in);
-        if (productDTO.getDescription() != null) {
-            System.out.println("Remove description (Y/N)?");
-            String inputAnswerToRemove = scanner.nextLine();
-            if (inputAnswerToRemove.equalsIgnoreCase("y")) {
+        System.out.println("Remove description (Y/N)?");
+        String inputAnswerToRemove = scanner.nextLine();
+        if (inputAnswerToRemove.equalsIgnoreCase("y")) {
+            if (productDTO.getDescription() != null) {
                 productDTO.setDescription(new ProductDTO().getDescription());
             }
         } else {
-            System.out.println("Please enter description (Y/N)?");
-            String inputAnswer = scanner.nextLine();
-            if (inputAnswer.equalsIgnoreCase("y")) {
-                System.out.println("Enter product description: ");
-                String description = scanner.nextLine();
-                for (int i = 0; i < description.length(); i++) {
-                    while (Character.isDigit(description.charAt(i))) {
-                        i = 0;
-                        System.out.println("Text must contain only letters.");
-                        System.out.print("Please enter product name: ");
-                        description = scanner.nextLine();
-                    }
+            System.out.println("Enter product description: ");
+            String description = scanner.nextLine();
+            for (int i = 0; i < description.length(); i++) {
+                while (Character.isDigit(description.charAt(i))) {
+                    i = 0;
+                    System.out.println("Text must contain only letters.");
+                    System.out.print("Please enter product name: ");
+                    description = scanner.nextLine();
                 }
-                productDTO.setDescription(description);
             }
+            productDTO.setDescription(description);
         }
     }
 
